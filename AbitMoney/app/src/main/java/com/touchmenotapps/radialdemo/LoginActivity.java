3001daaaced5efc59ced57c725820fbb27760999
rikimarutsui <rikimarutsui@storystate.com>
2015-03-26 20:40:57 +0800

    Add Context to the LoginActivity

--- AbitMoney/app/src/main/java/com/touchmenotapps/radialdemo/LoginActivity.java
+++ AbitMoney/app/src/main/java/com/touchmenotapps/radialdemo/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.touchmenotapps.radialdemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by rikimarutsui on 3/26/15.
@@ -29,16 +32,21 @@ public class LoginActivity extends Activity {
 			@Override
             public void onClick(View view){
                 if(view == loginBtn){
-                    startActivity(new Intent(LoginActivity.this, MainMenu.class));
+                    Context context = getApplicationContext();
+                    CharSequence text = "Login Successful!";
+                    int duration = Toast.LENGTH_LONG;
+                    final Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            toast.cancel();
+                            startActivity(new Intent(LoginActivity.this, MainMenu.class));
+                        }
+                    }, 1000);
                 }
             }
         });
 
     }
-
-    public void nextPage(View view){
-
-    }
-
-
 }
