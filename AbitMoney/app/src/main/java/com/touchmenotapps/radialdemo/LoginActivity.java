df74c9b53bcf3c173509d932e8edd6799df8a031
rikimarutsui <rikimarutsui@storystate.com>
2015-03-26 21:06:27 +0800

    Login Screen For Back button

--- AbitMoney/app/src/main/java/com/touchmenotapps/radialdemo/LoginActivity.java
+++ AbitMoney/app/src/main/java/com/touchmenotapps/radialdemo/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.touchmenotapps.radialdemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +20,21 @@ import android.widget.Toast;
 public class LoginActivity extends Activity {
     private Button loginBtn; // Login Button
     private TextView userAccount /* User Account */ , passcode /* Passcode */ ;
+    private DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    startActivity(new Intent(Intent.CATEGORY_HOME));
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    // do nothing
+                    break;
+            }
+        }
+    };
+
 
     @SuppressWarnings("serial")
     @Override
@@ -48,5 +66,19 @@ public class LoginActivity extends Activity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            builder.setMessage("Are you sure leave the app?");
+            builder.setPositiveButton("Yes", dialogClickListener);
+            builder.setNegativeButton("No", dialogClickListener);
+            builder.show();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
